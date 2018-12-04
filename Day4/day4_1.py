02a4c0f11eb96048accf72f8ed3311523e6f877e
klaa97 <10521251@polimi.it>
2018-12-04 20:00:20 +0100

    Added day4 part 2

--- Day4/day4_1.py
+++ Day4/day4_1.py
@@ -22,7 +22,7 @@ for i in logs.items():
     number_of_sleeps = defaultdict(int)
     currentmin = 0
     awake = False
-    for counter,signals in enumerate(i[1]):
+    for signals in i[1]:
         if (signals[1][0] == 'falls'):
             awake = False
             currentmin = signals[0][4]
@@ -35,14 +35,20 @@ for i in logs.items():
                 number_of_sleeps[j] = number_of_sleeps[j] + 1
             noawakeday=set()
     totalsleep[i[0]].append(number_of_sleeps)
-for i in totalsleep.items():
-    print(i)
 max_id = max(totalsleep,key = lambda x: totalsleep.get(x)[0])
+#Part 1
 print(max_id*max(totalsleep[max_id][1],key= lambda x: totalsleep[max_id][1].get(x)))
+#Part 2
+minute_for_guard = defaultdict(list)
+for i in totalsleep.items():
+    try: 
+        max_minute = max(i[1][1], key = lambda x: i[1][1].get(x))
+        minute_for_guard[i[0]].append(max_minute)
+        minute_for_guard[i[0]].append(i[1][1].get(max_minute))
+    except:
+        pass
+max_id_minute = max(minute_for_guard, key = lambda x: minute_for_guard.get(x)[1])
+max_minute = minute_for_guard[max_id_minute][0]
+print(max_id_minute*max_minute)
 
-
-
-        
-
-    
     
