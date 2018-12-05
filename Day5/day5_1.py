c92bb3c914249a26face7ccb008ec671e3c53875
klaa97 <39653693+klaa97@users.noreply.github.com>
2018-12-05 10:33:08 +0100

    Create day5_1.py

---
+++ Day5/day5_1.py
@@ -0,0 +1,9 @@
+with open("input","r") as fp:
+  s = fp.read()
+reacted = []
+for i in s:
+  if (len(reacted)!=0 and (reacted[-1] == chr(ord(i)-32) or (ord(i) <= 90 and reacted[-1] == chr(ord(i)+32)))):
+    reacted.pop()
+  else:
+    reacted.append(i)
+print(len(reacted))
