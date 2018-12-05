a849d6a5c0b373a3d067d943818a47d48d316ce3
klaa97 <10521251@polimi.it>
2018-12-05 14:34:13 +0100

    Fixed chr

--- Day5/day5_1.py
+++ Day5/day5_1.py
@@ -2,7 +2,7 @@ with open("input","r") as fp:
   s = fp.read()
 reacted = []
 for i in s:
-  if (len(reacted)!=0 and (reacted[-1] == chr(ord(i)-32) or (ord(i) <= 90 and reacted[-1] == chr(ord(i)+32)))):
+  if (len(reacted)!=0 and ((ord(i) > 90 and reacted[-1] == chr(ord(i)-32)) or (ord(i) <= 90 and reacted[-1] == chr(ord(i)+32)))):
     reacted.pop()
   else:
     reacted.append(i)
