1475191bae8542c88ee9522830f69dbda14abea4
klaa97 <10521251@polimi.it>
2018-12-06 10:31:43 +0100

    fixed gitignore

--- Day5/day5_2.py
+++ Day5/day5_2.py
@@ -6,7 +6,7 @@ for char in range(65,91):
   fixed = s.replace(chr(char),"").replace(chr(char+32),"")
   fullyreacted = []
   for j in fixed:
-    if (len(fullyreacted)!=0 and (fullyreacted[-1] == chr(ord(j)-32) or (ord(j) <= 90 and fullyreacted[-1] == chr(ord(j)+32)))):
+    if (len(fullyreacted)!=0 and ((ord(j) > 90 and fullyreacted[-1] == chr(ord(j)-32)) or (ord(j) <= 90 and fullyreacted[-1] == chr(ord(j)+32)))):
       fullyreacted.pop()
     else:
       fullyreacted.append(j)
